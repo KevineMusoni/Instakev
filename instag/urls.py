494c0652d74abc0f5fe2d01a10ae19a18c2d3484
Kevine Musoni <kevinenicky@gmail.com>
2018-12-14 15:43:49 +0300

    user authentication

--- instag/urls.py
+++ instag/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('^insta/$',views.insta,name='insta')
 ]
--- instag/views.py
+++ instag/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 # Create your views here.
-
+from django.contrib.auth.decorators import login_required
+from .forms import InstaLetterForm
 def welcome(request):
-    return HttpResponse('Instagram')
-    
+    return render(request, 'base.html')
+
+@login_required(login_url='/accounts/login/')
+def user(request, user_id):
+    try:
+        article = User.objects.get(id = user_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"registration/views.html", {"user":user})
+
+def insta(request):
+    if request.method == 'POST':
+        form = InstaLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = InstaLetterForm()
+    return render(request,'index.html')
