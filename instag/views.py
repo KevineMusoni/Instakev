7460dd2a629158792b6b36af7e688cd41d0e9727
Kevine Musoni <kevinenicky@gmail.com>
2018-12-14 15:55:28 +0300

    email configuration

--- instag/views.py
+++ instag/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-# Create your views here.
 from django.contrib.auth.decorators import login_required
 from .forms import InstaLetterForm
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+
 def welcome(request):
     return render(request, 'base.html')
 
@@ -15,10 +16,15 @@ def user(request, user_id):
     return render(request,"registration/views.html", {"user":user})
 
 def insta(request):
-    if request.method == 'POST':
+  if request.method == 'POST':
         form = InstaLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = InstaLetterRecipients(name = name,email =email)
+            recipient.save()
+            HttpResponseRedirect('insta')
     else:
         form = InstaLetterForm()
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
