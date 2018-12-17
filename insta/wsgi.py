74cf05e1ebab905562c20b7c19dd3c33f1304085
Kevine Musoni <kevinenicky@gmail.com>
2018-12-17 15:16:56 +0300

    procfile added

--- insta/wsgi.py
+++ insta/wsgi.py
@@ -14,4 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "insta.settings")
 
 application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
